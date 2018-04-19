bf00ce10e23d0ae576f76fa0e0750bf43247f951
KooMinJun <mjkoo711@gmail.com>
2018-04-20 00:40:22 +0900

    make draft that enroll for journal check page

--- src/OpenJournal/web/static/js/customize.js
+++ src/OpenJournal/web/static/js/customize.js
@@ -41,3 +41,50 @@ function ShowHideSubCat(){
   
   subElement.appendChild(df);
 }
+
+// tag 객체
+// var chip = {
+//   tag: 'chip content',
+//   image: '', //optional
+//   id: 1, //optional
+// };
+
+$('.chips').material_chip();
+$('.chips-initial').material_chip({
+  data: [{
+    tag: 'Apple',
+  }, {
+    tag: 'Microsoft',
+  }, {
+    tag: 'Google',
+  }],
+});
+$('.chips-placeholder').material_chip({
+  placeholder: 'Enter a tag',
+  secondaryPlaceholder: '+Tag',
+});
+$('.chips-autocomplete').material_chip({
+  autocompleteOptions: {
+    data: {
+      'Apple': null,
+      'Microsoft': null,
+      'Google': null
+    },
+    limit: Infinity,
+    minLength: 1
+  }
+});
+
+$('.chips').on('chip.add', function(e, chip){
+  // you have the added chip here
+});
+
+$('.chips').on('chip.delete', function(e, chip){
+  // you have the deleted chip here
+});
+
+$('.chips').on('chip.select', function(e, chip){
+  // you have the selected chip here
+});
+
+$('.chips-initial').material_chip('data');  
