99a2ca7b14b3292d13070987465429e78406807c
royeom <eom2390@gmail.com>
2018-05-17 20:45:04 +0900

    Modify about comment's feedback

--- src/OpenJournal/blockchain/src/js/customize.js
+++ src/OpenJournal/blockchain/src/js/customize.js
@@ -41,56 +41,9 @@ function ShowHideSubCat(){
   subElement.appendChild(df);
 }
 
-// tag 객체
-// var chip = {
-//   tag: 'chip content',
-//   image: '', //optional
-//   id: 1, //optional
-// };
-
 $(document).ready(function(){
   $('.dropdown-trigger').dropdown();
 });
-//
-// $('.chips').material_chip();
-// $('.chips-initial').material_chip({
-//   data: [{
-//     tag: 'Apple',
-//   }, {
-//     tag: 'Microsoft',
-//   }, {
-//     tag: 'Google',
-//   }],
-// });
-// $('.chips-placeholder').material_chip({
-//   placeholder: 'Enter a tag',
-//   secondaryPlaceholder: '+Tag',
-// });
-// $('.chips-autocomplete').material_chip({
-//   autocompleteOptions: {
-//     data: {
-//       'Apple': null,
-//       'Microsoft': null,
-//       'Google': null
-//     },
-//     limit: Infinity,
-//     minLength: 1
-//   }
-// });
-//
-// $('.chips').on('chip.add', function(e, chip){
-//   // you have the added chip here
-// });
-//
-// $('.chips').on('chip.delete', function(e, chip){
-//   // you have the deleted chip here
-// });
-//
-// $('.chips').on('chip.select', function(e, chip){
-//   // you have the selected chip here
-// });
-//
-// $('.chips-initial').material_chip('data');
 
 function openTotalJournal(){
   $('#journalPDF').attr('src', "../static/journal_example.pdf");
@@ -122,10 +75,3 @@ function off(){
   document.getElementById("ethereum_acc").value= value;
   document.getElementById("overlay").style.display = "none";
 }
-
-
-// function selectAccount(x){
-//   var value = x.innerText;
-//   document.getElementById("ethereum_acc").value= value;
-//   document.getElementById("overlay").style.display = "none";
-// }
