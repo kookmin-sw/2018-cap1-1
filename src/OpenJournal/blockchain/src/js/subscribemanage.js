599db0d28cccd33421b6fb98b5d3e080ab340992
royeom <eom2390@gmail.com>
2018-05-18 16:23:59 +0900

    Add javascript function

---
+++ src/OpenJournal/blockchain/src/js/subscribemanage.js
@@ -0,0 +1,37 @@
+var web3Provider;
+var contracts = {};
+
+if(typeof web3 !== "undefined"){
+    web3Provider = web3.currentProvider;
+}else {
+    web3Provider = new web3Provider.providers.HttpProvider("http://localhost:7545");
+}
+web3 = new Web3(web3Provider);
+
+function subscribeJournal(){
+    
+    $.getJSON("OpenJournal.json", function(data){
+        var Artifact = data;
+        contracts.OpenJournal = TruffleContract(Artifact);
+        contracts.OpenJournal.setProvider(web3Provider);
+        contracts.OpenJournal.deployed().then(function(instance){
+            var subscriber = getUserAccount();
+            var journalNumber = getJournalNumber();
+            console.log("subscriber : " + subscriber + ", number : " + journalNumber);
+            instance.subscribeJournal(journalNumber, { from: subscriber });
+        });
+    })
+}
+
+function getJournalNumber(){
+    var number = document.getElementById("journal_number").value;
+    return number;
+}
+
+function getUserAccount(){
+    var account;
+    web3.eth.getAccounts(function(err, accounts){
+        account = accounts[0];
+    });
+    return account;
+}
