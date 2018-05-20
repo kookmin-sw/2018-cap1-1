f9cc7431436ff06da787a27b9f6cac1365e3a079
achaccha <minjun12378@gmail.com>
2018-05-21 04:01:03 +0900

    Delete decoding code
    Signed-off-by: achaccha <minjun12378@gmail.com>

--- src/OpenJournal/web/route.py
+++ src/OpenJournal/web/route.py
@@ -664,43 +664,6 @@ def make_hash_string(journal_number, journal_title):        # number와 title을
     journal_hash = hashlib.sha256(new_str.encode('utf-8')).hexdigest()
 
     return journal_hash
-def unicodetoascii(text):
-
-    uni2ascii = {
-            ord('\xe2\x80\x99'.decode('utf-8')): ord("'"),
-            ord('\xe2\x80\x9c'.decode('utf-8')): ord('"'),
-            ord('\xe2\x80\x9d'.decode('utf-8')): ord('"'),
-            ord('\xe2\x80\x9e'.decode('utf-8')): ord('"'),
-            ord('\xe2\x80\x9f'.decode('utf-8')): ord('"'),
-            ord('\xc3\xa9'.decode('utf-8')): ord('e'),
-            ord('\xe2\x80\x9c'.decode('utf-8')): ord('"'),
-            ord('\xe2\x80\x93'.decode('utf-8')): ord('-'),
-            ord('\xe2\x80\x92'.decode('utf-8')): ord('-'),
-            ord('\xe2\x80\x94'.decode('utf-8')): ord('-'),
-            ord('\xe2\x80\x94'.decode('utf-8')): ord('-'),
-            ord('\xe2\x80\x98'.decode('utf-8')): ord("'"),
-            ord('\xe2\x80\x9b'.decode('utf-8')): ord("'"),
-            ord('\xe2\x80\xec'.decode('utf-8')): ord('"'),
-            ord('\xe2\x80\xed'.decode('utf-8')): ord('"'),
-
-            ord('\xe2\x80\x90'.decode('utf-8')): ord('-'),
-            ord('\xe2\x80\x91'.decode('utf-8')): ord('-'),
-
-            ord('\xe2\x80\xb2'.decode('utf-8')): ord("'"),
-            ord('\xe2\x80\xb3'.decode('utf-8')): ord("'"),
-            ord('\xe2\x80\xb4'.decode('utf-8')): ord("'"),
-            ord('\xe2\x80\xb5'.decode('utf-8')): ord("'"),
-            ord('\xe2\x80\xb6'.decode('utf-8')): ord("'"),
-            ord('\xe2\x80\xb7'.decode('utf-8')): ord("'"),
-
-            ord('\xe2\x81\xba'.decode('utf-8')): ord("+"),
-            ord('\xe2\x81\xbb'.decode('utf-8')): ord("-"),
-            ord('\xe2\x81\xbc'.decode('utf-8')): ord("="),
-            ord('\xe2\x81\xbd'.decode('utf-8')): ord("("),
-            ord('\xe2\x81\xbe'.decode('utf-8')): ord(")"),
-
-                            }
-    return text.decode('utf-8').translate(uni2ascii).encode('ascii')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
