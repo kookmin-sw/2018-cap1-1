cf118556930baddf60f11658be428c68dd635a28
ByunGooHoon <dksekfldks65@naver.com>
2018-05-28 18:03:33 +0900

    Complete Subscribing merge

--- src/src/js/subscribemanage.js
+++ src/src/js/subscribemanage.js
@@ -8,28 +8,31 @@ if(typeof web3 !== "undefined"){
 }
 web3 = new Web3(web3Provider);
 
-function subscribeJournal(callback){
+function subscribeJournal(_id){
 
     $.getJSON("OpenJournal.json", function(data){
         var Artifact = data;
         contracts.OpenJournal = TruffleContract(Artifact);
         contracts.OpenJournal.setProvider(web3Provider);
         contracts.OpenJournal.deployed().then(function(instance){
-            var subscriber = getUserAccount();
-            var journalNumber = getJournalNumber();
-            instance.subscribeJournal(journalNumber, { from: subscriber });
-            alert("subscriber : " + subscriber + ", number : " + journalNumber + "\n구독이 완료되기까지 1분이 소요될 수 있습니다.");
-        });
+        	var journalNumber = getJournalNumber();
+        	var subscriberAccount = getSubscriberAccount();    
+		instance.subscribeJournal(journalNumber, { from: subscriberAccount });
+        	alert("subscriber : " + subscriberAccount + ", number : " + journalNumber + "\n구독이 완료되기까지 1분이 소요될 수 있습");
+        	location.href ="subscribeState?data="+_id+",1,"+journalNumber;
+	});
     });
     
 }
 
 function getJournalNumber(){
-    var number = document.getElementById("journal_number").value;
+    var number = parseInt(document.getElementById("journal_number").value);
+    console.log("number: " + number);
+    console.log("type : "+ typeof(number));
     return number;
 }
 
-function getUserAccount(){
+function getSubscriberAccount(){
     var account;
     web3.eth.getAccounts(function(err, accounts){
         account = accounts[0];
